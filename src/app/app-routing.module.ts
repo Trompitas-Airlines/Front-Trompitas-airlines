9a4066ab2c24407fa8d5f5d639705a1ad7096f96
Roman <romancastiblancoc@gmail.com>
2023-04-27 15:05:07 -0500

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardAdminComponent } from './components/dashboard-admin/dashboard-admin.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { MenuUsuarioComponent } from './components/menu-usuario/menu-usuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'', redirectTo: 'home', pathMatch: 'full'},
+{path:'home', component: HomeComponent},
+{path:'login', component: LoginComponent},
+{path: 'home', redirectTo: 'home', pathMatch: 'full'},
+{path:'dashboard-admin', component:DashboardAdminComponent},
+{path:'admin-boton', component: LoginComponent},
+{path:'menu-usuario', component: MenuUsuarioComponent},
+{path:'siguiente-boton', component: MenuUsuarioComponent},
+{path: 'dashboard-boton', component: DashboardAdminComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/menu-usuario/menu-usuario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-usuario',
+  templateUrl: './menu-usuario.component.html',
+  styleUrls: ['./menu-usuario.component.css']
+})
+export class MenuUsuarioComponent {
+
+
+}
